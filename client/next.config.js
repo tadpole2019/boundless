9d091b97ebf08ebd2afcf1183f028bdc22ccb84a
jhan1998 <48130038+tadpole2019@users.noreply.github.com>
2024-06-24 20:32:58 +0800

    public

---
+++ client/next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    domains: ['via.placeholder.com', 'localhost', 'lh3.googleusercontent.com'],
+  },
+
+  // comment for render twice issue
+  // avoid cors with proxy
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'http://localhost:3005/:path*', // Proxy to Backend
+  //     },
+  //   ]
+  // },
+}
+
+module.exports = nextConfig
